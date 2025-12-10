3ab7f2711e270d3e61aed6f66033ff9e09ef5029
jm <kjmin065999@naver.com>
2025-12-10 15:25:01 +0900

    refactor: api 버전 관리를 위한 prefix 적용

--- BE/config/urls.py
+++ BE/config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("accounts.urls")),
-    path("posts/", include("posts.urls")),
-    path("routes/", include("routes.urls")),
+    path("api/v1/auth/", include("accounts.urls")),
+    path("api/v1/posts/", include("posts.urls")),
+    path("api/v1/routes/", include("routes.urls")),
 ]
